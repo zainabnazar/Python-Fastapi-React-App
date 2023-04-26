bfd5a9ffc7ee3359652e7d635dfd383b5d7a4bd4
zainabnoori <zainab.noori@and.digital>
2023-04-26 10:08:09 +0100

    Add the quiz logic

--- backend/main.py
+++ backend/main.py
@@ -7,14 +7,17 @@ app = FastAPI()
 
 class question(BaseModel):
     id:int
-    text: str
+    questionText: str
+    questionAnswers: List[str]
+    correct:str
+
+
 
 DB: list[question]=[
-    question(id=1, text="question 1"),
-    question(id=2, text="question 2"),
-    question(id=3, text="question 3"),
-    question(id=4, text="question 4"),
-
+    question(id=1, questionText="What is the capital of France?",questionAnswers=["Paris","London","Russia"],correct="Paris"),
+    question(id=2, questionText="How many planets in the solar system?",questionAnswers=["10","13","16"],correct="13"),
+    question(id=3, questionText="Which country launched the world's largest carbon market on February 1, 2021?",questionAnswers=["China","USA","Germany"],correct="USA"),
+    question(id=4, questionText="In March 2021, which company announced that it would start producing COVID-19 vaccines for its rival, Johnson & Johnson?",questionAnswers=["Pfizer","Moderna","AstraZeneca"],correct="Pfizer"),
 ]
 
 @app.get("/api")
